31fb218f51f9291f5285106ea7434dae8844ab5a
colbyfayock <fayock@gmail.com>
2022-05-02 11:35:26 -0300

    canarias event

--- src/data/events.js
+++ src/data/events.js
@@ -15,6 +15,26 @@ export const events = {
 
   // Events
 
+  canariasjs: {
+    register: {
+      link: 'https://forms.monday.com/forms/e5f543d53ee8de0441cf133d13df553b?r=use1',
+      text: 'Get Your Free Account',
+    },
+    hashtags: ['JSDayCAN2022'],
+    incentive: {
+      text: 'extra 3 credits',
+      moreInfo: {
+        text: 'what are credits?',
+        link: 'https://cloudinary.com/pricing/compare-plans',
+      },
+    },
+    utm: {
+      source: 'canariasjs',
+      medium: 'event',
+      campaign: '2022canariasjs_booth',
+    },
+  },
+
   cityjsbrazil: {
     register: {
       link: 'https://forms.monday.com/forms/e1ad68a7699011ccfd2514a8d47a3aa3',
